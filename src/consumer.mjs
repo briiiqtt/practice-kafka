b4021113fcaeebb922ba83de6f3b8ab3c64ec885
briiiqtt <briiiqtt@gmail.com>
2025-08-04 23:52:42 +0900

    feat: modulize consumer; change topic to 'partition-test'

--- src/consumer.mjs
+++ src/consumer.mjs
@@ -1,29 +1,25 @@
 import { Kafka } from 'kafkajs';
 
-const groupIdArgv = process.argv.find(el => el.toLowerCase().includes('groupid='));
-const groupId = groupIdArgv.split("=")[1];
+export const poll = async (clientId, topic, groupId) => {
+  const kafka = new Kafka({
+    clientId, // 이건 그냥 디버깅용 식별자
+    brokers: ['localhost:9092'],
+  });
 
-console.log('groupId:', groupId);
-
-const kafka = new Kafka({
-  clientId: 'my-consumer-1',
-  brokers: ['localhost:9092'],
-});
-
-const consumer = kafka.consumer({ groupId: `test-group-${groupId}` });
-
-const run = async () => {
+  const consumer = kafka.consumer({ groupId });
+  /*
+   * group의 역할
+   * 1. 중복을 방지함. (한 그룹 내에서 한 개의 consumer만 메시지를 가져감)
+   * 2. 그룹 id를 다르게 주면 한 메시지에 대해 분리된 여러 처리가 가능
+   * 3. offset(메시지 어디까지 읽었는 지)은 그룹별로 관리됨.
+   */
   await consumer.connect();
- 
-  const topic = 'my-topic-1';
 
   await consumer.subscribe({ topic, fromBeginning: true });
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
-      console.log(`${message.value.toString()} | ${partition} | ${topic}`);
+      console.log(`${topic}/${partition}/${clientId}: ${message.value.toString()}`);
     },
   });
 };
-
-run().catch(console.error);
--- src/producer.mjs
+++ src/producer.mjs
@@ -1,7 +1,7 @@
 import { Kafka } from 'kafkajs';
 
 const kafka = new Kafka({
-  clientId: 'my-producer-1',
+  clientId: 'my-producer-1', // 이건 그냥 디버깅용 식별자
   brokers: ['localhost:9092'],
 });
 
@@ -10,20 +10,18 @@ const producer = kafka.producer();
 export const produce = async (msg) => {
   await producer.connect();
 
-  const topic = 'my-topic-1';
-  
-  const key = 'someKey';
+  const topic = 'partition-test';
   /*
   * key의 역할?
   * 1. 어느 파티션으로 갈 지를 결정함 (key 없으면 round-robin, 있으면 hash(key) % partition 수)
   * 2. 같은 key는 항상 같은 파티션으로 가기 때문에 순서가 보장됨
   *     -> user 로그의 순서를 보장하고 싶으면 key를 userId로 지정하면 되것지요
   */
-  const messages = [{ key, value: msg }];
+  const messages = [{ value: msg }];
 
   await producer.send({ topic, messages });
 
-  console.log(messages.length, 'messages sent.');
+  console.log('sent message:', msg);
 
   await producer.disconnect();
 };
